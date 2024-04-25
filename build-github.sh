df0e31c33f2922bd3b4aa4549f91561c78cd66d7
ryzen's tech! <158189251+ryzenstechdev@users.noreply.github.com>
2024-04-25 18:29:07 +0200

    Update build-github.sh

--- build-github.sh
+++ build-github.sh
@@ -5,30 +5,30 @@ script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 source_dir=$script_dir/LingmoSrcBuild/Src
 deb_dir=$script_dir/LingmoSrcBuild/Deb
 
-echo '欢迎使用灵墨OS自动编译脚本!'
-echo '提示: 使用前请确认已经安装了所有必要的依赖。'
+echo 'Welcome to astraOS Builder!'
+echo 'NOTE: Powered by CutefishOS and LingmoOS Sources'
 
 if test -e $source_dir
 then
-  echo '存在同名的LingmoOS源文件夹，正在删除...'
+  echo 'Deleting existing astraOS WorkDir...'
   rm -rf $source_dir
 fi
-echo '创建新的LingmoOS源文件夹...'
+echo 'Creating existing astraOS WorkDir...'
 mkdir -p $source_dir
 
 if test -e $deb_dir
 then
-  echo '存在同名的LingmoOS输出文件夹，正在删除...'
+  echo 'Wait a second...'
   rm -rf $deb_dir
 fi
-echo '创建新的LingmoOS输出文件夹...'
+echo 'Wait YET ANOTHER second ASSHOLE...'
 mkdir -p $deb_dir
 
 function InstallDepends() {
-    echo '开始安装依赖'
+    echo 'Installing Dependences... (i like dick :3)'
     apt-get --yes install git  devscripts equivs
-    rm -rfv LingmoOSBuildDeps
-    git clone https://github.com/LingmoOS/LingmoOSBuildDeps.git
+    rm -rfv astraOSBuildDeps
+    git clone https://github.com/teamsakuraos/astraOSBuildDeps.git
     cd LingmoOSBuildDeps
     mk-build-deps -i -t "apt-get -y" -r  > /dev/null
 }
@@ -36,14 +36,14 @@ function InstallDepends() {
 # 定义一个函数来编译项目
 function Compile() {
     repo_name=$1
-    echo "开始编译 $repo_name ..."
+    echo "Cloning $repo_name ..."
     cd $source_dir
     if test -d $repo_name; then
         echo "已存在 $repo_name 目录，更新中..."
         cd $repo_name && git reset --hard HEAD && git pull
     else
         echo "正在克隆 $repo_name ..."
-        git clone https://github.com/LingmoOS/$repo_name.git
+        git clone https://github.com/teamsakuraos/$repo_name.git
         cd $repo_name
     fi
     echo "正在安装 $repo_name 依赖..."
@@ -64,37 +64,37 @@ function Compile() {
     cd $source_dir
     mv -v !(*dbgsym*).deb $deb_dir/
 }
-REPOS="lingmo-screenlocker lingmo-settings lingmo-screenshots lingmo-cursor-themes lingmo-sddm-theme lingmo-appmotor lingmo-neofetch lingmo-daemon lingmo-ocr lingmo-terminal lingmo-gtk-themes LingmoUI lingmo-systemicons lingmo-wallpapers lingmo-debinstaller lingmo-calculator lingmo-launcher lingmo-statusbar lingmo-qt-plugins lingmo-dock liblingmo lingmo-filemanager lingmo-core lingmo-texteditor lingmo-kwin-plugins lingmo-videoplayer"
+REPOS="astra-screenlocker astra-settings astra-screenshots lingmo-cursor-themes astra-sddm-theme astra-appmotor lingmo-neofetch lingmo-daemon lingmo-ocr astra-terminal astra-gtk-themes PetalyUI astra-systemicons astra-wallpapers lingmo-debinstaller astra-calculator astra-launcher astra-statusbar lingmo-qt-plugins astra-dock liblingmo lingmo-filemanager lingmo-core astra-texteditor lingmo-kwin-plugins lingmo-videoplayer"
 
 # 先安装依赖
 InstallDepends
 
 # 列出所有项目供用户选择
 select project in \
-lingmo-screenlocker \
-lingmo-settings \
-lingmo-screenshots \
+astra-screenlocker \
+astra-settings \
+astra-screenshots \
 lingmo-cursor-themes \
-lingmo-sddm-theme \
-lingmo-appmotor \
+astra-sddm-theme \
+astra-appmotor \
 lingmo-neofetch \
 lingmo-daemon \
 lingmo-ocr \
-lingmo-terminal \
-lingmo-gtk-themes \
-LingmoUI \
-lingmo-systemicons \
-lingmo-wallpapers \
+astra-terminal \
+astra-gtk-themes \
+PetalyUI \
+astra-systemicons \
+astra-wallpapers \
 lingmo-debinstaller \
-lingmo-calculator \
-lingmo-launcher \
-lingmo-statusbar \
+astra-calculator \
+astra-launcher \
+astra-statusbar \
 lingmo-qt-plugins \
-lingmo-dock \
+astra-dock \
 liblingmo \
 lingmo-filemanager \
 lingmo-core \
-lingmo-texteditor \
+astra-texteditor \
 lingmo-kwin-plugins \
 lingmo-videoplayer \
 all \
